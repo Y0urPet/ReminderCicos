ad0ccdc4d70166da6b11bc37df2f4baf148ca295
timothy <tandrian24@bsd.idserve.net>
2024-05-13 14:10:28 +0700

    Initial Commit

---
+++ BasicWatch Watch App/BasicWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicWatchApp.swift
+//  BasicWatch Watch App
+//
+//  Created by Timothy Andrian on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BasicWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasicWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BasicWatch Watch App
+//
+//  Created by Timothy Andrian on 13/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
